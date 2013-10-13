f1f8b9ee1eab4b28f5d9b59cf2e1f4cf5df73d9d
Lucas Bickel <hairmare@purplehaze.ch>
2013-10-13 21:49:28 +0200

    scripts for composer
    call npm and grunt to download and build graviphoton.

---
+++ src/GravityPlatform/Composer/ScriptHandler.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * script handler for composer scripts
+ */
+
+namespace GravityPlatform\Composer;
+
+use Composer\Script\CommandEvent;
+use Symfony\Component\Process\Process;
+
+/**
+ * ScriptHandler
+ *
+ * Handles call from composer to third party package management systems.
+ *
+ * @category Composer
+ * @package  Gravity-Platform
+ * @author   Lucas Bickel <lucas.bickel@swisscom.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.com
+ */
+class ScriptHandler
+{
+    /**
+     * location where graviphoton is installed by npm
+     *
+     * @const String
+     */
+    const GRAVIPHOTON_DIR = 'node_modules/graviphoton';
+
+    /**
+     * call npm install
+     *
+     * @param CommandEvent $event composer event
+     *
+     * @return void
+     */
+    public static function installNpmModules(CommandEvent $event)
+    {
+	self::runCommand('npm install');
+    }
+
+    /**
+     * call npm update
+     *
+     * @param CommandEvent $event composer event
+     *
+     * @return void
+     */
+    public static function updateNpmModules(CommandEvent $event)
+    {
+        self::runCommand('npm update');
+    }
+
+    /**
+     * call npm install --dev-all in GRAVIPHOTON_DIR
+     *
+     * @param CommandEvent $event composer event
+     *
+     * @return void
+     */
+    public static function installGraviphoton(CommandEvent $event)
+    {
+	self::runCommand('npm install --dev-all', self::GRAVIPHOTON_DIR);
+    }
+
+    /**
+     * call npm update --dev-all in GRAVIPHOTON_DIR
+     *
+     * @param CommandEvent $event composer event
+     *
+     * @return void
+     */
+    public static function updateGraviphoton(CommandEvent $event)
+    {
+        self::runCommand('npm update --dev-all', self::GRAVIPHOTON_DIR);
+    }
+
+    /**
+     * call vendorized grunt in GRAVIPHOTON_DIR
+     *
+     * @param CommandEvent $event composer event
+     *
+     * @return void
+     */
+    public static function buildGraviphoton(CommandEvent $event)
+    {
+	self::runCommand('./node_modules/grunt-cli/bin/grunt', self::GRAVIPHOTON_DIR);
+    }
+
+    /**
+     * run a command
+     *
+     * @param String $cmd command
+     * @param String $cwd command working directory
+     *
+     * @return void
+     */
+    public static function runCommand($cmd, $cwd = null)
+    {
+        $process = new Process($cmd, $cwd, null, null, 0);
+	$process->run(function ($type, $buffer) { echo $buffer; });
+
+	if (!$process->isSuccessful()) {
+	    throw new \RuntimeException('An error occurred while running '.$cmd);
+	}
+    }
+}
