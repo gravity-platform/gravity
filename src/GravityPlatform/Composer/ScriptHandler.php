cbea73ed4e8e5f28df4ace97e5a25e6001ff2fa6
Lucas Bickel <hairmare@purplehaze.ch>
2013-10-20 15:49:34 +0200

    copy graviton and graviphoton to final locations
    This is where the puppet config done by vagrant is going to expect these files.

--- src/GravityPlatform/Composer/ScriptHandler.php
+++ src/GravityPlatform/Composer/ScriptHandler.php
@@ -137,6 +137,74 @@ class ScriptHandler
     }
 
     /**
+     * copy app/ and web/ contents from graviton
+     *
+     * @param CommandEvent $event composer event
+     *
+     * @return void
+     */
+    public static function installGraviton(CommandEvent $event)
+    {
+        $extras = $event->getComposer()->getPackage()->getExtra();
+        $appDir = $extras['symfony-app-dir'];
+        $webDir = $extras['symfony-web-dir'];
+        $gravitonDir = __DIR__.'/../../../vendor/graviton/graviton';
+
+        if (!is_dir($webDir)) {
+            mkdir($webDir);
+        }
+        copy($gravitonDir.'/web/app.php', $webDir.'/app.php');
+        copy($gravitonDir.'/web/app_dev.php', $webDir.'/app_dev.php');
+        copy($gravitonDir.'/web/check.php', $webDir.'/check.php');
+
+        if (!is_dir($appDir)) {
+            mkdir($appDir);
+        }
+        copy($gravitonDir.'/app/AppKernel.php', $appDir.'/AppKernel.php');
+        copy($gravitonDir.'/app/autoload.php', $appDir.'/autoload.php');
+        copy($gravitonDir.'/app/console', $appDir.'/console');
+        if (!is_dir($appDir.'/config')) {
+            mkdir($appDir.'/config');
+        }
+        copy($gravitonDir.'/app/config/config.xml', $appDir.'/config/config.xml');
+        copy($gravitonDir.'/app/config/config_dev.xml', $appDir.'/config/config_dev.xml');
+        copy($gravitonDir.'/app/config/parameters.xml', $appDir.'/config/parameters.xml');
+        copy($gravitonDir.'/app/config/parameters_local.xml', $appDir.'/config/parameters_local.xml');
+        copy($gravitonDir.'/app/config/routing.xml', $appDir.'/config/routing.xml');
+        copy($gravitonDir.'/app/config/security.xml', $appDir.'/config/security.xml');
+    }
+
+    /**
+     * copy finished graviphoton to local web/ dir
+     *
+     * @param CommandEvent $event composer event
+     *
+     * @return void
+     */
+    public static function installGraviphotonFiles(CommandEvent $event)
+    {
+        $extras = $event->getComposer()->getPackage()->getExtra();
+        $webDir = $extras['symfony-web-dir'];
+        $graviphotonDir = __DIR__.'/../../../node_modules/graviphoton/dist';
+
+        if (!is_dir($webDir)) {
+            mkdir($webDir);
+        }
+        copy($graviphotonDir.'/index.html', $webDir.'/index.html');
+        copy($graviphotonDir.'/graviphoton.min.css', $webDir.'/graviphoton.min.css');
+        copy($graviphotonDir.'/graviphoton.min.js', $webDir.'/graviphoton.min.js');
+        copy($graviphotonDir.'/graviphoton.less', $webDir.'/graviphoton.less');
+        if (!is_dir($webDir.'/font')) {
+            mkdir($webDir.'/font');
+        }
+        copy($graviphotonDir.'/font/FontAwesome.otf', $webDir.'/font/FontAwesome.otf');
+        copy($graviphotonDir.'/font/fontawesome-webfont.svg', $webDir.'/font/fontawesome-webfont.svg');
+        copy($graviphotonDir.'/font/fontawesome-webfont.ttf', $webDir.'/font/fontawesome-webfont.ttf');
+        copy($graviphotonDir.'/font/fontawesome-webfont.eot', $webDir.'/font/fontawesome-webfont.eot');
+        copy($graviphotonDir.'/font/fontawesome-webfont.woff', $webDir.'/font/fontawesome-webfont.woff');
+    }
+
+    /**
      * run a command
      *
      * @param String $cmd command
